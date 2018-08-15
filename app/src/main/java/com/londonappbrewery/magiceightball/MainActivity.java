34feedf0419f8799f8fa55961207dfa93439ad30
Lenovo <mrsi16cs@cmrit.ac.in>
2018-08-15 13:59:25 +0530

    completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.nio.InvalidMarkException;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button askbutton=(Button) findViewById(R.id.ask_button);
+        final ImageView imageview=(ImageView) findViewById(R.id.imageview);
+        final int[] array={R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        askbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r=new Random();
+                int x=r.nextInt(5);
+                imageview.setImageResource(array[x]);
+
+            }
+        });
+
+
+
     }
 }
